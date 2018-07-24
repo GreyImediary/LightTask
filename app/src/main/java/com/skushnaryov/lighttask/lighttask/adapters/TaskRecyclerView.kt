33511270aa841038ff21e35a7e61a0e9f372e1ed
Skushnaryov <ser.kushnaryov@gmail.com>
2018-07-25 00:20:50 +0300

    add listener for subtask's checkboxes

--- app/src/main/java/com/skushnaryov/lighttask/lighttask/adapters/TaskRecyclerView.kt
+++ app/src/main/java/com/skushnaryov/lighttask/lighttask/adapters/TaskRecyclerView.kt
@@ -1,10 +1,10 @@
 package com.skushnaryov.lighttask.lighttask.adapters
 
 import android.content.Context
-import android.util.Log
 import android.view.View
 import android.view.ViewGroup
 import android.view.animation.AnimationUtils
+import android.widget.CompoundButton
 import androidx.core.view.isVisible
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -12,6 +12,7 @@ import com.skushnaryov.lighttask.lighttask.*
 import com.skushnaryov.lighttask.lighttask.db.Task
 import kotlinx.android.synthetic.main.item_task.view.*
 import java.util.*
+import kotlin.math.ceil
 
 class TaskRecyclerView : RecyclerView.Adapter<TaskRecyclerView.TaskHolder>() {
     var list: List<Task> = emptyList()
@@ -28,11 +29,29 @@ class TaskRecyclerView : RecyclerView.Adapter<TaskRecyclerView.TaskHolder>() {
     override fun onBindViewHolder(holder: TaskHolder, position: Int) = holder.bind(list[position])
 
     class TaskHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        private val innerAdapter = SubtaskRecyclerView()
 
         fun bind(task: Task) = with(itemView) {
             taskName_textView.text = task.name
             taskDate_textView.text = getStringDate(task.date)
+
+            val innerAdapter = SubtaskRecyclerView(CompoundButton.OnCheckedChangeListener { _, onChecked ->
+                val subtaskPercent = ceil(100F / task.listOfSubtasks.size).toInt()
+                if (onChecked) {
+                    val finalPercent = getCurrentPercent() + subtaskPercent
+                    taskPercent_textView.text = context.getString(R.string.percentFormat, finalPercent)
+                } else {
+                    val finalPercent = getCurrentPercent() - subtaskPercent
+                    taskPercent_textView.text = context.getString(R.string.percentFormat, finalPercent)
+                }
+
+                if (getCurrentPercent() > 100) {
+                    taskPercent_textView.text = context.getString(R.string.percentFormat, 100)
+                }
+
+                if (getCurrentPercent() < 0) {
+                    taskPercent_textView.text = context.getString(R.string.percentFormat, 0)
+                }
+            })
 
             if (!task.groupName.isEmpty()) {
                 taskGroup_textView.visible()
@@ -55,16 +74,20 @@ class TaskRecyclerView : RecyclerView.Adapter<TaskRecyclerView.TaskHolder>() {
                     if (!rv_subtasks.isVisible) {
                         rv_subtasks.visible()
                         fadeInOutAnimation(context, rv_subtasks)
-                        taskArrow_textView.text = resources.getString(R.string.subtasks_arrow_up)
+                        taskArrow_textView.text = context.getString(R.string.subtasks_arrow_up)
                         fadeInOutAnimation(context, taskArrow_textView)
                     } else {
                         rv_subtasks.gone()
-                        taskArrow_textView.text = resources.getString(R.string.subtasks_arrow_down)
+                        taskArrow_textView.text = context.getString(R.string.subtasks_arrow_down)
                         fadeInOutAnimation(context, taskArrow_textView)
                     }
                 }
             }
         }
+
+        private fun getCurrentPercent() = itemView.taskPercent_textView.text
+                .trim('%')
+                .toString().toInt()
 
         private fun fadeInOutAnimation(context: Context, view: View) {
             var anim = AnimationUtils.loadAnimation(context, R.anim.fade_out)
