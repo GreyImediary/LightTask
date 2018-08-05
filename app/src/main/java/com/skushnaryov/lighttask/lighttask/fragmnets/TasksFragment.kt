dad7d3c116b926ad52f6d91e5e8e1fe117111250
Skushnaryov <ser.kushnaryov@gmail.com>
2018-08-05 16:01:56 +0300

    imports of TaskFragment refactored

--- app/src/main/java/com/skushnaryov/lighttask/lighttask/fragmnets/TasksFragment.kt
+++ app/src/main/java/com/skushnaryov/lighttask/lighttask/fragmnets/TasksFragment.kt
@@ -1,12 +1,9 @@
 package com.skushnaryov.lighttask.lighttask.fragmnets
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
-import androidx.core.view.isEmpty
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
@@ -20,9 +17,6 @@ import com.skushnaryov.lighttask.lighttask.gone
 import com.skushnaryov.lighttask.lighttask.viewModels.TaskViewModel
 import com.skushnaryov.lighttask.lighttask.visible
 import kotlinx.android.synthetic.main.fragment_tasks.*
-import kotlinx.coroutines.experimental.CommonPool
-import kotlinx.coroutines.experimental.android.UI
-import kotlinx.coroutines.experimental.async
 import kotlinx.coroutines.experimental.launch
 import org.jetbrains.anko.contentView
 import java.util.*
