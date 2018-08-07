4f00692fb94efc4bc427cd9e0c353fc297b05465
Skushnaryov <ser.kushnaryov@gmail.com>
2018-08-07 19:29:13 +0300

    layout for AddActivity added

--- app/src/main/java/com/skushnaryov/lighttask/lighttask/activities/AddActivity.kt
+++ app/src/main/java/com/skushnaryov/lighttask/lighttask/activities/AddActivity.kt
@@ -1,12 +1,18 @@
 package com.skushnaryov.lighttask.lighttask.activities
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
 import com.skushnaryov.lighttask.lighttask.R
+import com.skushnaryov.lighttask.lighttask.inflateMenu
 
 class AddActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add)
+        window.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_VISIBLE)
     }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean = inflateMenu(R.menu.add_activity_menu, menu)
 }
--- app/src/main/java/com/skushnaryov/lighttask/lighttask/commons.kt
+++ app/src/main/java/com/skushnaryov/lighttask/lighttask/commons.kt
@@ -1,8 +1,11 @@
 package com.skushnaryov.lighttask.lighttask
 
 import android.view.LayoutInflater
+import android.view.Menu
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.MenuRes
+import androidx.appcompat.app.AppCompatActivity
 
 fun ViewGroup.inflate(resId: Int, attachToRoot: Boolean = false): View =
         LayoutInflater.from(context).inflate(resId, this, attachToRoot)
@@ -10,3 +13,9 @@ fun ViewGroup.inflate(resId: Int, attachToRoot: Boolean = false): View =
 fun View.visible() { this.visibility = View.VISIBLE }
 fun View.invisible() { this.visibility = View.INVISIBLE }
 fun View.gone() { this.visibility = View.GONE }
+
+
+fun AppCompatActivity.inflateMenu(@MenuRes resId: Int, menu: Menu): Boolean {
+    menuInflater.inflate(resId, menu)
+    return true
+}
